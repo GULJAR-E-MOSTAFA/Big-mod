99c61d3ee7a3ad9357af9839a685c52fd632eaf7
GULJAR-E-MOSTAFA <160768728+GULJAR-E-MOSTAFA@users.noreply.github.com>
2024-05-17 05:41:47 +0600

    Add files via upload

---
+++ Bib Mod .cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int  bigmod( int base, int power,int mod)
+{
+    if(power == 0)
+        return 1;
+    else if ( power %2== 1 )
+    {
+         int a =base % mod;
+         int b = (bigmod (base , power-1 , mod))% mod;
+
+             return ( a*b)%mod;
+         }
+         else if ( power % 2 == 0 )
+         {
+             int a =( bigmod ( base , power/2,mod)) % mod;
+             return ( a*a) % mod;
+         }
+}
+
+ int main()
+{
+ int base,power,mod;
+ while(cin>> base>>power>> mod)
+
+     cout<<bigmod ( base, power,mod)<< endl;
+     cout<<endl;
+      return 0;
+
+
+}
